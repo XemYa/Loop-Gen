0a26fa62490be48d27c62b944c702687e462e6bc
Alex <126931036+XemYa@users.noreply.github.com>
2025-03-18 12:34:48 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+("-------Loop Number-------")
+number = input("Pick a number for the loop:")
+if not number.isdigit():
+    print("Needs to be a number");
+    exit()
+number=int(number)
+print("-------Loop Text-------")
+text = input("Text:")
+print("-------Loop Result-------")
+for i in range(number):
+    print(f"{i +1}.{text}")
